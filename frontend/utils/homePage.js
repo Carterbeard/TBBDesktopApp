c18db65fc0c191fd03a075e7dbb29c44472f07c6
Carterbeard <105600062+Carterbeard@users.noreply.github.com>
2026-02-07 03:13:59 +0000

    Parameter Input & View Change when file is added
    Created a fitting design skeleton ready for when parameters are ready to be introduced into the system. When a file is added to the application it now changes showing the name of the file. Alongside this, there is now a remove file button that can remove the given inputted file before processing.

--- frontend/utils/homePage.js
+++ frontend/utils/homePage.js
@@ -1,3 +1,6 @@
+const inputFile = document.getElementById("inputFile");
+const dropZone = document.querySelector(".dragAndDrop");
+
 function toggleMenu() {
     document.querySelector(".sideMenu").classList.toggle("active");
     }
@@ -7,3 +10,36 @@ function processData() {
     localStorage.removeItem("progressComplete");
     window.location.href = "analysis.html?runProgress=true";
 }
+
+function toggleParameters() {
+    document.querySelector(".parameterInput").classList.toggle("active", inputFile.files.length>0);
+}
+
+function toggleWhenActiveFile(){
+    const fileName = inputFile.files[0].name;
+
+    document.getElementById("uploadImg").classList.toggle("active");
+    document.getElementById("dragP").innerHTML= "File added: " + fileName;
+    document.getElementById("inputBtnSection").hidden = true;
+    document.getElementById("removeFileBtn").hidden = false;
+}
+
+function removeFile(){
+    inputFile.value=""
+    document.getElementById("uploadImg").classList.toggle("active");
+    document.getElementById("dragP").innerHTML= "Drag and Drop to Upload File";
+    document.getElementById("inputBtnSection").hidden = false;
+    document.getElementById("removeFileBtn").hidden = true;
+}
+
+inputFile.addEventListener("change",()=>{
+    toggleParameters()
+    toggleWhenActiveFile()
+});
+dropZone.addEventListener("dragover", e => e.preventDefault());
+dropZone.addEventListener("drop", e => {
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    toggleParameters();
+    toggleWhenActiveFile();
+});
